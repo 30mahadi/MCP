5d5c4ce0477d6bdc068ad61d18641757db432cda
Mahadi Mokbul Ali <125756057+30mahadi@users.noreply.github.com>
2026-08-08 23:21:44 +0600

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,18 @@
-# Note: You can use any Debian/Ubuntu based image you want.
-FROM mcr.microsoft.com/devcontainers/base:ubuntu
+FROM mcr.microsoft.com/dotnet/runtime:7.0 AS base
+WORKDIR /app
 
-# [Optional] Uncomment this section to install additional OS packages.
-# RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
-#     && apt-get -y install --no-install-recommends <your-package-list-here>
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["util/seed-memory/seed-memory.csproj", "util/seed-memory/"]
+RUN dotnet restore "util/seed-memory/seed-memory.csproj"
+COPY . .
+WORKDIR "/src/util/seed-memory"
+RUN dotnet build "seed-memory.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "seed-memory.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "seed-memory.dll"]
--- Program (2).cs
+++ Program (2).cs
@@ -1,12 +1,45 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Program.cs
 
-namespace AutoGen.Anthropic.Sample;
+using System.Runtime.CompilerServices;
+using AutoGen.Core;
+using AutoGen.WebAPI;
 
-internal static class Program
+var alice = new DummyAgent("alice");
+var bob = new DummyAgent("bob");
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+// run endpoint at port 5000
+builder.WebHost.UseUrls("http://localhost:5000");
+var app = builder.Build();
+
+app.UseAgentAsOpenAIChatCompletionEndpoint(alice);
+app.UseAgentAsOpenAIChatCompletionEndpoint(bob);
+
+app.Run();
+
+public class DummyAgent : IStreamingAgent
 {
-    public static async Task Main(string[] _)
+    public DummyAgent(string name = "dummy")
     {
-        await Anthropic_Agent_With_Prompt_Caching.RunAsync();
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public async Task<IMessage> GenerateReplyAsync(IEnumerable<IMessage> messages, GenerateReplyOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return new TextMessage(Role.Assistant, $"I am dummy {this.Name}", this.Name);
+    }
+
+    public async IAsyncEnumerable<IMessage> GenerateStreamingReplyAsync(IEnumerable<IMessage> messages, GenerateReplyOptions? options = null, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var reply = $"I am dummy {this.Name}";
+        foreach (var c in reply)
+        {
+            yield return new TextMessageUpdate(Role.Assistant, c.ToString(), this.Name);
+        }
     }
 }
