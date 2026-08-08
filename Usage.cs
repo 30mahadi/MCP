13d0d8e474c636aac8ba390841dc266eb548d431
Mahadi Mokbul Ali <125756057+30mahadi@users.noreply.github.com>
2026-08-08 23:53:08 +0600

    Add files via upload

--- Usage.cs
+++ Usage.cs
@@ -1,26 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Usage.cs
 
-using System.Text.Json.Serialization;
+namespace Microsoft.AutoGen.AgentChat.Abstractions;
 
-namespace AutoGen.Mistral;
-
-public class Usage
+public struct RequestUsage
 {
-    [JsonPropertyName("prompt_tokens")]
     public int PromptTokens { get; set; }
-
-    /// <summary>
-    /// Gets or Sets CompletionTokens
-    /// </summary>
-    /// <example>93</example>
-    [JsonPropertyName("completion_tokens")]
     public int CompletionTokens { get; set; }
-
-    /// <summary>
-    /// Gets or Sets TotalTokens
-    /// </summary>
-    /// <example>107</example>
-    [JsonPropertyName("total_tokens")]
-    public int TotalTokens { get; set; }
 }
